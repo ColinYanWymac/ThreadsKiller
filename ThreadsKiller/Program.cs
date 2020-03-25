59b82058e6a5da1f150435f1a6f59ed9a434cf77
Colin <colin.yan@wymac.com.au>
2020-03-26 09:45:38 +1100

    Push to git

---
+++ ThreadsKiller/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadsKiller
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            KillThread("ModelDemo");        // ModelDemo
+            KillThread("MonoLauncher");     // MonoLauncher
+            KillThread("msvsmon");          // Visual Studio 2015 Remote Debugger
+            Console.WriteLine("Program will automatically close soon.");
+            Thread.Sleep(500);
+        }
+
+        private static void KillThread(string argName)
+        {
+            Process[] processItems = Process.GetProcessesByName(argName);
+            if (processItems.Length == 0)
+            {
+                Console.WriteLine($"Cannot find process \"{argName}\"");
+                return;
+            }
+
+            foreach (Process processItem in processItems)
+            {
+                processItem.Kill();
+            }
+            Console.WriteLine($"{processItems.Length} \"{argName}\" killed.");
+        }
+
+    }
+}
---
+++ ThreadsKiller/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ThreadsKiller")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadsKiller")]
+[assembly: AssemblyCopyright("Copyright © WyMac 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec0c244a-354d-4488-9a3e-fe32c5ba4142")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
